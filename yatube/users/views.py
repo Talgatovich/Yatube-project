da804edc288cac9a556cafb8af1e864f55fc1d28
Раиль Ибятов <93610314+Talgatovich@users.noreply.github.com>
2023-05-03 11:19:31 +0300

    Update views.py

--- yatube/users/views.py
+++ yatube/users/views.py
@@ -1,11 +1,5 @@
-# Импортируем CreateView, чтобы создать ему наследника
 from django.views.generic import CreateView
-
-# Функция reverse_lazy позволяет получить URL по параметрам функции path()
-# Берём, тоже пригодится
 from django.urls import reverse_lazy
-
-# Импортируем класс формы, чтобы сослаться на неё во view-классе
 from .forms import CreationForm
 
 
@@ -14,4 +8,3 @@ class SignUp(CreateView):
     # После успешной регистрации перенаправляем пользователя на главную.
     success_url = reverse_lazy('posts:index')
     template_name = 'users/signup.html'
-# Create your views here.
